71d8936bf32f8b4bab60844a266b4d361f5c4b97
Ehrazhussain <EHRAZRAJA9@GMAIL.COM>
2024-08-03 01:18:54 +0500

    Mern-Stack-Event-Project

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        hello welcome to our website, we are a team of event planners, we are here to help you plan your event, we have a wide range of services, from event planning to event management, we can help you plan your event and make it a success. We have a team of experienced event planners who can help you plan your event, from the planning stage to the day of the event. We can help you plan your event and make it a success.
+      Event planners organise various corporate and social events, including launch parties, conferences, festivals and expositions. They develop ideas, book venues, manage budgets and coordinate with suppliers to produce successful events that align with the vision and expectations of client
+      </p>
+    </section>
+  );
+};
+
+export default About;
